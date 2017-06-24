8b3419d81403dfd9f9beaabc7e39c72f4d8f9871
Encore Shao <encore.shao@gmail.com>
2017-06-24 20:38:00 +0800

    MISC - Misc updates

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,5 +1,7 @@
 module DashboardsHelper
-  def top_ten_comments
-    Comment.parent_comments.order('created_at DESC').limit(10)
+  def top_ten_articles_with_top_comments
+    article_ids = Comment.parent_comments.group("commentable_id").order("COUNT(*) DESC").count.keys.first(10)
+
+    Article.select('title, created_at, published_at, permalink').where(id: article_ids)
   end
 end
