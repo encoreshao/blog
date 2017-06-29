f84b3110b78369e05c4dc4f3db924c89458e7621
Encore Shao <encore.shao@gmail.com>
2017-06-29 16:51:40 +0800

    MISC - Update image path

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,15 @@
 module ApplicationHelper
-	def banner_iamge_url
-		"/assets/photos/banner-00#{1+rand(6)}.jpg"
-	end
+  def random_banner_image
+    = content_tag :img, src: "photos/banner-00#{1+rand(6)}.jpg", class: 'attachment-blog-image size-blog-image wp-post-image'
+  end
+
+  def author_thumb_image
+    = content_tag :img, src: "photos/encore.jpg", class: 'attachment-small-thumb size-small-thumb wp-post-image'
+  end
+
+  def author_avatar
+    = content_tag :img, src: "photos/encore.jpg", class: 'avatar avatar-80 photo'
+  end
 
   def widget_categories
     Rails.cache.fetch("icmoc_widget_categories_with_#{params[:locale]}", expires_in: 1.hours) do
